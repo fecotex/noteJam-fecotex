603e08bc8391c790e4c966176392bd0a46eb4023
fecotex@gmail.com <fecotex@gmail.com>
2017-05-02 11:47:22 -0300

    secure ssl redirect 02

--- notejam/settings.py
+++ notejam/settings.py
@@ -120,6 +120,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'djangosecure.middleware.SecurityMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -143,6 +144,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangosecure',
     #'django.contrib.admin',
     'pads',
     'notes',
